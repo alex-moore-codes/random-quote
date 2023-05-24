ef9e8cb39373656ce0eae8d6da98febd9ee8fe10
alex-moore-codes <alex.moore.codes@gmail.com>
2023-05-24 19:44:05 -0400

    added not ok response throw error

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@
 import { useEffect, useState } from 'react';
 import QuoteBlock from './components/QuoteBlock';
 import RandomButton from './components/RandomButton';
+import { error } from 'console';
 
 export type Result = {
   _id: string;
@@ -19,9 +20,14 @@ export default function () {
     const response = await fetch(
       'https://quote-garden.onrender.com/api/v3/quotes/random'
     );
-    const json = await response.json();
-    const { data } = json;
-    setQuote(data);
+
+    if (!response.ok) {
+      throw new Error('Response from API not ok.');
+    } else {
+      const json = await response.json();
+      const { data } = json;
+      setQuote(data);
+    }
   }
 
   useEffect(() => {
